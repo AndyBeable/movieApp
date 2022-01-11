29fc61d5b0958096b3d9b1f48b7d51be090d12b3
AndyBeable <andy.beable@gmail.com>
2022-01-11 16:38:27 +0000

    font awesome icons

--- src/components/ui/Navigation.vue
+++ src/components/ui/Navigation.vue
@@ -1,15 +1,21 @@
 <template>
-  <h1>
-    <span>Movies</span>App
-  </h1>
+  <div class="navbar">
+    <fa icon="bars" />
+    <h1><span>Movies</span>App</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Navigation.vue"
+  name: "Navigation.vue",
 };
 </script>
 
 <style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
 
 </style>
